89ab4a5952b2a75efd606d65aed806bc0cafd833
Theodore Bagwell <theodore@bagwell.com>
2018-01-25 19:23:26 -0500

    to slow to slow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,16 @@ require 'rvg/rvg'
 
 class PagesController < ApplicationController
   include ESpeak
+  before_action :set_slug, only: [:audio, :image, :meaning]
+  before_action :truncate_slug, only: [:audio, :image]
 
   def home;end
 
   def audio
-    send_data ESpeak::Speech.new(params[:slug]).bytes(), type: 'audio/mpeg', disposition: 'inline'
+    send_data ESpeak::Speech.new(@slug).bytes(), type: 'audio/mpeg', disposition: 'inline'
   end
 
   def image
-    @slug = params[:slug].upcase
     format = 'png'
     width, height = 2000,500
     color = '#343a40'
@@ -35,10 +36,18 @@ class PagesController < ApplicationController
   end
 
   def meaning
-    @slug = params[:slug].upcase
     @lp_num = (@slug.hash % 10).to_s
     @slug_link = "<a href='#{@site_info.meaning_url % {slug: @slug}}'>#{@slug}</a>"
     @site_contents = @site_info.site_contents
     @lp_content = @site_contents.find_by(title: @lp_num)
   end
+
+  private
+    def set_slug
+      @slug = params[:slug].upcase
+    end
+
+    def truncate_slug
+      @slug = @slug.truncate(10)
+    end
 end
